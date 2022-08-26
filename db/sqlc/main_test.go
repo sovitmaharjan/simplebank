7e57ee1bd44b45349a2e8d8180965a288fefd4d2
sovitmaharjan <sovitmaharjan@gmail.com>
2022-08-26 13:37:28 +0545

    random

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:--49@localhost:5433/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	testQueries = New(conn)
+	m.Run()
+}
